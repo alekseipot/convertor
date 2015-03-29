ed16370931c8160729466321bc947dd0bddb4610
Alexey Potapov <aleksei.pot@gmail.com>
2015-03-29 12:14:08 +0300

    init logger

--- src/main/java/com/smartelephant/convertor/rest/ConvertorController.java
+++ src/main/java/com/smartelephant/convertor/rest/ConvertorController.java
@@ -1,5 +1,7 @@
 package com.smartelephant.convertor.rest;
 
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,9 +10,13 @@ import javax.ws.rs.core.Response;
 @Path("/convert")
 public class ConvertorController {
 
+    private static Logger LOG = Logger.getLogger(ConvertorController.class.getName());
+
     @GET
     @Path("/{param}")
     public Response printMessage(@PathParam("param") String msg) {
+
+        LOG.info("Got param in request: " + msg);
 
         String result = "Restful example : " + msg;
 
