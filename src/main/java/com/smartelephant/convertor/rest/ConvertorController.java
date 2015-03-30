1322aff5e330d3afe78748d657d3cd5e6d02f1d2
lakseipot <aleksei.pot@gmail.com>
2015-03-30 16:45:07 +0300

    implement REST endpoint for file uploading process

--- src/main/java/com/smartelephant/convertor/rest/ConvertorController.java
+++ src/main/java/com/smartelephant/convertor/rest/ConvertorController.java
@@ -1,11 +1,17 @@
 package com.smartelephant.convertor.rest;
 
 import org.apache.log4j.Logger;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import org.apache.commons.io.IOUtils;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 @Path("/convert")
 public class ConvertorController {
@@ -24,4 +30,61 @@ public class ConvertorController {
 
     }
 
+    @POST
+    @Path("/upload/{requestedPage}")
+    @Consumes("multipart/form-data")
+    public Response uploadFile(MultipartFormDataInput input, @PathParam("requestedPage") final Integer requestedPage) {
+
+        LOG.info("Got request for uploading source file.");
+        LOG.info("Requested page: " + requestedPage);
+        String fileName = "";
+
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+        List<InputPart> inputParts = uploadForm.get("uploadedFile");
+
+        for (InputPart inputPart : inputParts) {
+
+            try {
+
+                MultivaluedMap<String, String> header = inputPart.getHeaders();
+                fileName = getFileName(header);
+                LOG.info("Source file name: " + fileName);
+                //convert the uploaded file to inputstream
+                InputStream inputStream = inputPart.getBody(InputStream.class, null);
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        LOG.info("Build file for response.");
+        String fileContent = "Source file name: " + fileName + ". Requested page: " + requestedPage;
+        Response.ResponseBuilder response = Response.ok(fileContent);
+        response.header("Content-Disposition", String.format("attachment; filename=\"%s.txt\"", fileName.substring(0, fileName.indexOf("."))));
+        return response.build();
+
+    }
+
+    /**
+     * header sample
+     * {
+     * Content-Type=[image/png],
+     * Content-Disposition=[form-data; name="file"; filename="filename.extension"]
+     * }
+     */
+    //get uploaded filename, is there a easy way in RESTEasy?
+    private String getFileName(MultivaluedMap<String, String> header) {
+
+        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("filename"))) {
+
+                String[] name = filename.split("=");
+
+                return name[1].trim().replaceAll("\"", "");
+            }
+        }
+        return "unknown";
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,30 @@
 <html>
+<head>
+    <script type="text/javascript" src="js/jquery/jquery-2.1.3.js"></script>
+
+    <script>
+        function sendRequest() {
+            var path = $("#source-file-form").attr("action") + $("#requestedPage").val().trim();
+            $("#source-file-form").attr("action", path);
+            $("#source-file-form").submit(); //Submit the form
+        }
+    </script>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h1>Upload your file</h1>
+
+<form id="source-file-form" action="/convertor/engine/convert/upload/" method="post" enctype="multipart/form-data">
+
+    <p>
+        Select a file : <input type="file" name="uploadedFile"/>
+    </p>
+
+    <p>
+        Input page number : <input type="text" id="requestedPage" name="requestedPage"/>
+    </p>
+
+    <input type="button" onclick="return sendRequest();" value="Convert It"/>
+
+</form>
 </body>
 </html>
